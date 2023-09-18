79cc71420cbb670bc53370f97d5a326c511eff88
AnnKrolikowska <krolikowska.ann@gmail.com>
2023-09-18 19:49:39 +0200

    added content to offer section

--- src/sections/Offer.jsx
+++ src/sections/Offer.jsx
@@ -1,19 +1,50 @@
 import React from 'react';
-// import iconFirst from '../images/icon1.webp'
-// import iconSecond from '../images/icon2.webp'
-// import iconThird from '../images/icon3.webp'
+import backgroundImg from '../images/background.png';
+import iconFirst from '../images/icon1.png'
+import iconSecond from '../images/icon2.png'
+import iconThird from '../images/icon3.png'
 
 function Offer() {
   return (
     <section>
-        <div className='container mx-auto auto pb-10 md:mb-32'>
+      <div className=' bg-cover bg-center'  style={{
+            backgroundImage: `url(${backgroundImg})`,
+          }}>
+        <div className='container mx-auto auto pb-10 px-5 xl:px-0 md:mb-32 pt-16'>
             <div className='relative inline-block'>
                 <h2 className='text-5xl font-radley relative z-10'>Oferta</h2>
                 <div className='w-100 h-4 bg-primary-25 absolute bottom-[-1px] left-0 right-0 rounded-basic'></div> 
             </div>
-            <div className='md:flex justify-between'>
-                
+            <div className='lg:flex gap-8 xl:gap-24 my-8 text-center justify-between'>
+                <div className="rounded-basic lg:w-[350px] mb-8 p-10 bg-white shadow-md">
+                  <h3 className='text-2xl'>Zajęcia indywidualne</h3>
+                  <img src={iconFirst} alt="individual" className='mx-auto py-8'/>
+                  <ul>
+                    <li>Lekcja online - <span className='font-semibold'>60</span> zł/h</li>
+                    <li>Lekcja offline - <span className='font-semibold'>70</span> zł/h</li>
+                  </ul>
+                  <p className='mt-8'>Pierwsza lekcja dla zajęć indywidualnych o 50% taniej</p>
+                </div>
+                <div className="rounded-basic lg:w-[350px] mb-8 p-10 bg-white shadow-md">
+                  <h3 className='text-2xl'>Zajęcia w parach</h3>
+                  <img src={iconSecond} alt="individual" className='mx-auto py-8'/>
+                  <ul>
+                    <li>Lekcja online - <span className='font-semibold'>90</span> zł/h</li>
+                    <li>Lekcja offline - <span className='font-semibold'>100 </span>zł/h</li>
+                  </ul>
+                  <p className='mt-8'></p>
+                </div>
+                <div className="rounded-basic lg:w-[350px] mb-8 p-10 bg-white shadow-md">
+                  <h3 className='text-2xl'>Grupa początkująca</h3>
+                  <img src={iconThird} alt="individual" className='mx-auto py-8'/>
+                  <ul>
+                    <li>Dla 4-6 osób</li>
+                    <li><span className='font-semibold'>45</span> zł/ 1.5 h x 2 tyg</li>
+                  </ul>
+                  <p className='mt-8'>godzina 18:00 - 19:30, wtorek, piątek</p>
+                </div>
             </div>
+      </div>
       </div>
     </section>
   );
